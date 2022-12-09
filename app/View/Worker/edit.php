14858b7424bd43cf715eb405e9aed54bba2faf75
Said Dinar Fadilah <saiddinarfadilah@gmail.com>
2022-12-09 11:33:32 +0700

    feature edit and delete worker

---
+++ app/View/Worker/edit.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <form method="post" action="/worker/update">
+        <div class="row mb-3 mt-5">
+            <label for="inputId" class="col-md-2 col-form-label">Id Worker</label>
+            <div class="col-md-4">
+                <input type="text" class="form-control" id="inputId" name="id" disabled value="<?= $model["worker"]["id"] ?>">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="inputName" class="col-md-2 col-form-label">Name</label>
+            <div class="col-md-4">
+                <input type="text" class="form-control" id="inputName" name="name">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="inputIdBranch" class="col-md-2 col-form-label">Id Branch</label>
+            <div class="col-md-4">
+                <input type="text" class="form-control" id="inputIdBranch" name="branchidbranch">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="inputPosition" class="col-md-2 col-form-label">Position</label>
+            <div class="col-md-4">
+                <input type="text" class="form-control" id="inputPosition" name="position">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-secondary">Add</button>
+    </form>
+</div>
--- public/index.php
+++ public/index.php
@@ -9,5 +9,9 @@ use CodingTest\php\Controller\WorkerController;
 Router::add("GET","/worker", WorkerController::class, "index" );
 Router::add("GET","/worker/add", WorkerController::class, "formAdd" );
 Router::add("POST","/worker/add", WorkerController::class, "postAdd" );
+Router::add("GET","/worker/edit", WorkerController::class, "formUpdate" );
+Router::add("POST","/worker/edit", WorkerController::class, "postUpdate" );
+
+Router::add("GET","/worker/delete", WorkerController::class, "destroy" );
 
 Router::run();
